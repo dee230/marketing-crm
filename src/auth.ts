8d9be6a7d3149e1e7f9e8fd7393340030111595b
Denzel <denzelrooke@live.com>
2026-04-23 08:26:29 -0400

    Fix cookies config for Vercel

--- src/auth.ts
+++ src/auth.ts
@@ -43,6 +43,17 @@ export const authConfig: NextAuthOptions = {
     }),
   ],
   trustHost: true,
+  cookies: {
+    sessionToken: {
+      name: `next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: process.env.NODE_ENV === 'production',
+      },
+    },
+  },
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
